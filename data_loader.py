4add519c3b03f6bd47024ff55f819c816d1e29e4
wheezydeeeb <tridibjdas@gmail.com>
2024-06-04 19:25:30 +0000

    Updated data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -70,7 +70,7 @@ def get_data_loader(num_classes=100, dataset_dir="/home/khincho/distillers/datas
 
     train_transform = transforms.Compose([
         # transforms.RandomCrop(32, padding=4),
-        transforms.Resize([40, 40]),
+        transforms.Resize([48, 48]),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         normalize,
