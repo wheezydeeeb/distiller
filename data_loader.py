f019b64a11c6d837254176db6d62c8185bedc327
wheezydeeeb <tridibjdas@gmail.com>
2024-06-20 09:45:50 +0000

    Configured for FERPlus Dataset.

--- data_loader.py
+++ data_loader.py
@@ -103,6 +103,8 @@ def get_data_loader(num_classes=100, dataset_dir="/home/khincho/distillers/datas
                           transform=test_transform)
     elif num_classes == 7:
         testset = dataset("/home/khincho/data/FERPlus/val/", transform=test_transform)
+    elif num_classes == 8:
+        testset = dataset("/home/khincho/data/FERPlus/val/", transform=test_transform)
 
     train_loader = torch.utils.data.DataLoader(trainset,
                                                batch_size=batch_size,
